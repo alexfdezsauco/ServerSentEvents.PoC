3e71126a42a84ebd8f863abffce986e0c611481c
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-12-26 11:28:03 -0500

    Small improvements

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+builder.Logging.AddConsole();
 
 var app = builder.Build();
+var logger = app.Logger;
 
 // Configure the HTTP request pipeline.
 
@@ -11,19 +14,22 @@ app.UseStaticFiles();
 app.Use(
     async (context, next) =>
         {
-            if (context.Request.Path.ToString().Equals("/notifications-hub"))
+            if (context.Request.Path.Value == "/notifications-hub")
             {
                 var response = context.Response;
                 response.Headers.Add("Content-Type", "text/event-stream");
 
                 var count = 0;
-                while (true)
+                StreamWriter streamWriter = new StreamWriter(response.Body);
+                while (!context.RequestAborted.IsCancellationRequested)
                 {
-                    await response.WriteAsync("data:");
-                    await response.WriteAsync($"Notification {++count} at {DateTime.Now}");
-                    await response.WriteAsync("\r\r");
+                    var data = $"Notification {++count} at {DateTime.Now}";
 
-                    await response.Body.FlushAsync();
+                    await streamWriter.WriteLineAsync($"data:{data}");
+                    await streamWriter.WriteLineAsync();
+                    await streamWriter.FlushAsync();
+
+                    logger.LogInformation($"Sent: {data}");
 
                     await Task.Delay(TimeSpan.FromSeconds(1));
                 }
